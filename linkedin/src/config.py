906e096859f87b70c01685202794780862d56afa
Allainclair <allainclair@gmail.com>
2025-01-03 16:59:46 -0300

    Upload

--- linkedin/src/config.py
+++ linkedin/src/config.py
@@ -3,10 +3,13 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 EMAIL = config.get("EMAIL")
 PASSWORD = config.get("PASSWORD")
-SESSION_STORAGE = config.get("SESSION_STORAGE")
 
+# Playwright
+MODE = "no-login"
+LINKEDIN_HOME = "https://www.linkedin.com"
 URL_LOGIN = "https://www.linkedin.com/uas/login"
-
 LINKEDIN_KEYWORDS = ["python"]
 LOCATION = "Brazil"
 LINKEDIN_URL_SEARCH = f"https://www.linkedin.com/jobs/search/?keywords={" ".join(LINKEDIN_KEYWORDS)}&location={LOCATION}&refresh=false"
+USER_DATA_DIR = 'playwright-context-data/'
+MAX_TRIES = 20
--- linkedin/src/main.py
+++ linkedin/src/main.py
@@ -1,11 +1,14 @@
 from asyncio import run, sleep
 from playwright.async_api import Page, async_playwright
-from src.config import URL_LOGIN, EMAIL, PASSWORD, LINKEDIN_URL_SEARCH, SESSION_STORAGE
+from playwright._impl._errors import Error
+from src.config import URL_LOGIN, EMAIL, PASSWORD, LINKEDIN_URL_SEARCH, USER_DATA_DIR, LINKEDIN_HOME, MAX_TRIES
+import os
+from rich import print
 
 
 async def login(page: Page, email: str, password: str) -> Page:
 	await page.goto(URL_LOGIN)
-	await page.wait_for_load_state("load")
+	await page.wait_for_load_state("domcontentloaded")
 
 	await page.get_by_label("Email or phone").click()
 	await page.get_by_label("Email or phone").fill(email)
@@ -20,18 +23,64 @@ async def login(page: Page, email: str, password: str) -> Page:
 	return page
 
 
+async def linkedin_load_search(page: Page) -> Page:
+	for _ in range(MAX_TRIES):
+		try:
+			await page.goto(LINKEDIN_URL_SEARCH)
+			await page.wait_for_load_state("networkidle")
+			title = await page.title()
+			print(f"Title of LINKEDIN_URL_SEARCH: {title}")
+			if "Jobs in Brazil".lower() in title.lower():
+				return page
+		except Error as e:
+			print(e)
+			return page
+
+
+def start_new_context() -> bool:
+	if not os.path.exists(USER_DATA_DIR):
+		os.mkdir(USER_DATA_DIR)
+		return True
+	return False
+
+
 async def main() -> None:
 	async with async_playwright() as p:
-		user_data_dir = 'playwright-context-data/'
-		browser = await p.chromium.launch_persistent_context(user_data_dir, headless=False)
-		# browser = await p.chromium.launch(headless=False)
+		start_new_context()
+
+		browser = await p.chromium.launch_persistent_context(USER_DATA_DIR, headless=False)
 		page = await browser.new_page()
+
+		await linkedin_load_search(page)
+
+		# await page.goto(LINKEDIN_URL_SEARCH)
+		# # await page.wait_for_load_state()
+		# await page.wait_for_load_state("networkidle")
+		# title = await page.title()
+		#
+		# if "Jobs in Brazil" not in title:
+		# 	await page.goto(LINKEDIN_URL_SEARCH)
+		# 	await page.wait_for_load_state("networkidle")
+
+		elements = await page.query_selector_all('.base-card')
+		print(elements)
+		print(len(elements))
+
+		# Get the list of job ids
+		# job_ids = [element.get_attribute('data-occludable-job-id') for element in elements]
+		# print(job_ids)
+
+		await sleep(120)
+
+		# browser = await p.chromium.launch_persistent_context(USER_DATA_DIR, headless=False)
+		# browser = await p.chromium.launch(headless=False)
+		# page = await browser.new_page()
 
 		# page = await login(page, EMAIL, PASSWORD)
 		# await sleep(10)
-
-		await page.goto(LINKEDIN_URL_SEARCH)
-		await sleep(20)
+		#
+		# await page.goto(LINKEDIN_URL_SEARCH)
+		# await sleep(20)
 
 
 if __name__ == "__main__":
